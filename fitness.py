23710d26f583431ea5fb07c363d5a7f5d4c398f9
Girgis Sami <jirgissamy78@gmail.com>
2024-11-27 18:30:56 +0200

    add property method to route_distance

--- fitness.py
+++ fitness.py
@@ -1,12 +1,10 @@
-from city import City
-
-
 class Fitness:
     def __init__(self, route):
         self.route = route
         self.distance = 0
         self.fitness = 0.0
 
+    @property
     def route_distance(self):
         self.distance = self.path_distance if self.distance == 0 else self.distance
         return self.distance
@@ -30,16 +28,5 @@ class Fitness:
 
     def route_fitness(self):
         if self.fitness == 0:
-            self.fitness = 1 / float(self.route_distance())
+            self.fitness = 1 / float(self.route_distance)
         return self.fitness
-
-
-city1 = City(1, 5)
-city2 = City(4, 9)
-city3 = City(7, 8)
-city4 = City(3, 1)
-
-fitt = Fitness([city1, city2, city3, city4])
-
-print(fitt.route_distance())
-print(fitt.route_fitness())
