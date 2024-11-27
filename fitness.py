27c748ab9a5f0c91a8d5562b9885b917686e2975
Girgis Sami <jirgissamy78@gmail.com>
2024-11-27 18:19:53 +0200

    change var declaration

--- fitness.py
+++ fitness.py
@@ -18,13 +18,13 @@ class Fitness:
 
     @property
     def path_distance(self):
-        path_distance = 0
+        result = 0
         for i, from_city in enumerate(self.route):
             next_index = i + 1
             to_city = self.next_city(next_index, self.route)
-            path_distance += from_city.distance(to_city)
-        self.distance = path_distance
-        return path_distance
+            result += from_city.distance(to_city)
+        self.distance = result
+        return result
 
     def next_city(self, next_index, route):
         result = None
