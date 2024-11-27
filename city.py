5b8caf8dd82801e53dce2b4dfbede8187da0b29e
Girgis Sami <jirgissamy78@gmail.com>
2024-11-27 16:28:21 +0200

    add City class

---
+++ city.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class City:
+    def __init__(self, x, y):
+        self._x_coordinate = x
+        self._y_coordinate = y
+
+    def distance(self, city):
+        x_distance = abs(self.get_x - city.get_x)
+        y_distance = abs(self.get_x, city.get_y)
+        result = np.sqrt((x_distance**2) + (y_distance**2))
+        return result
+
+    @property
+    def get_x(self):
+        return self._x_coordinate
+
+    @property
+    def get_y(self):
+        return self._y_coordinate
