8aa0fdd92e94b99eb35d1655c7aee1eed2edd699
Girgis Sami <jirgissamy78@gmail.com>
2024-11-28 14:33:21 +0200

    add some test

--- selection.py
+++ selection.py
@@ -1,5 +1,7 @@
 import operator
 from fitness import Fitness
+from city import City
+from population import initial_population
 
 
 def rank_routes(population):
@@ -12,3 +14,14 @@ def fitness_results(population):
     for i, _ in enumerate(population):
         results[i] = Fitness(population[i]).route_fitness()
     return results
+
+
+city1 = City(3, 5)
+city2 = City(3, 6)
+city3 = City(23, 5)
+city4 = City(8, 65)
+city5 = City(35, 25)
+
+cityList = [city1, city2, city3, city4, city5]
+pop = initial_population(10, cityList)
+print(rank_routes(pop))
