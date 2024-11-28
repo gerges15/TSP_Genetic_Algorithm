b8af1d9eec99eae614090340a6db139b58f711ca
Girgis Sami <jirgissamy78@gmail.com>
2024-11-28 15:59:02 +0200

    add selection function

--- selection.py
+++ selection.py
@@ -1,4 +1,7 @@
 import operator
+import random
+import numpy as np
+import pandas as pd
 from fitness import Fitness
 from city import City
 from population import initial_population
@@ -16,6 +19,30 @@ def fitness_results(population):
     return results
 
 
+def selection(pop_ranked, elite_size):
+    selection_results = []
+
+    # create dataRankFrame from population rank list that contains index and corresponding fitness
+    df = pd.DataFrame(np.array(pop_ranked), columns=["Index", "Fitness"])
+    # create cumulative fitness column
+    df["cum_sum"] = df.Fitness.cumsum()
+    # convert cumulative fitness to percentage
+    df["cum_perc"] = 100 * df.cum_sum / df.Fitness.sum()
+
+    for i in range(0, elite_size):
+        selection_results.append(pop_ranked[i][0])
+    for i in range(0, len(pop_ranked) - elite_size):
+        # pick from 0 to 100 random number
+        pick = 100 * random.random()
+        for i, _ in enumerate(pop_ranked):
+            # i => row and iat[row, column] where 3 is a percent column
+            if pick <= df.iat[i, 3]:
+                # index => pop_ranked[i][0]
+                selection_results.append(pop_ranked[i][0])
+                break
+    return selection_results
+
+
 city1 = City(3, 5)
 city2 = City(3, 6)
 city3 = City(23, 5)
@@ -24,4 +51,6 @@ city5 = City(35, 25)
 
 cityList = [city1, city2, city3, city4, city5]
 pop = initial_population(10, cityList)
-print(rank_routes(pop))
+ranked = rank_routes(pop)
+print(ranked)
+print(selection(ranked, 10))
