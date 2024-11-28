e2c694c6906aa3c4cb9c84723ca14caa3a978d9d
Girgis Sami <jirgissamy78@gmail.com>
2024-11-28 14:21:26 +0200

    change function declaration

--- selection.py
+++ selection.py
@@ -4,11 +4,11 @@ from fitness import Fitness
 
 def rank_routes(population):
     return sorted(
-        fitnesses_result(population).items(), key=operator.itemgetter(1), reverse=True
+        fitness_results(population).items(), key=operator.itemgetter(1), reverse=True
     )
 
 
-def fitnesses_result(population):
+def fitness_results(population):
     results = {}
     for i, _ in enumerate(population):
         results[i] = Fitness(population[i]).route_fitness()
