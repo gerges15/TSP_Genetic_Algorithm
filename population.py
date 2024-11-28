1ed53806184ae696231ff2a33453e19457d9945e
Girgis Sami <jirgissamy78@gmail.com>
2024-11-28 13:42:43 +0200

    change var declaration

--- population.py
+++ population.py
@@ -6,8 +6,8 @@ def create_route(city_list):
 
 
 def initial_population(pop_size, city_list):
-    population = []
+    result = []
 
     for _ in range(0, pop_size):
-        population.append(create_route(city_list))
-    return population
+        result.append(create_route(city_list))
+    return result
