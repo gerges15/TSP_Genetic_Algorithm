4bfb085062d7617438e762016a0702cd99ed2b82
Girgis Sami <jirgissamy78@gmail.com>
2024-11-28 14:18:43 +0200

    extract fitnesses_result function

--- selection.py
+++ selection.py
@@ -3,7 +3,12 @@ from fitness import Fitness
 
 
 def rank_routes(population):
+    fitness_results = fitnesses_result(population)
+    return sorted(fitness_results.items(), key=operator.itemgetter(1), reverse=True)
+
+
+def fitnesses_result(population):
     fitness_results = {}
     for i, _ in enumerate(population):
         fitness_results[i] = Fitness(population[i]).route_fitness()
-    return sorted(fitness_results.items(), key=operator.itemgetter(1), reverse=True)
+    return fitness_results
