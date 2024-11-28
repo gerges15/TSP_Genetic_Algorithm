b5a4d6e917e53221b553d9253db86bf9cf206ba4
Girgis Sami <jirgissamy78@gmail.com>
2024-11-28 13:36:44 +0200

    remove deaded code

--- population.py
+++ population.py
@@ -2,7 +2,6 @@ import random
 
 
 def create_route(city_list):
-    route = random.sample(city_list, len(city_list))
     return random.sample(city_list, len(city_list))
 
 
@@ -12,8 +11,3 @@ def initial_population(pop_size, city_list):
     for _ in range(0, pop_size):
         population.append(create_route(city_list))
     return population
-
-
-arr = [1, 2, 3, 4, 5, 6]
-print(create_route(arr))
-print(initial_population(10, arr))
