d27c2c9d2c35ac02e357afe3cb43b12b6b68f45a
Girgis Sami <jirgissamy78@gmail.com>
2024-11-29 19:26:41 +0200

    change function declaration

--- mutation.py
+++ mutation.py
@@ -4,11 +4,19 @@ import random
 def mutate(individual, mutation_rate):
     for swapped, _ in enumerate(individual):
         if random.random() < mutation_rate:
-            swap_with = int(random.random() * len(individual))
-
-            city1 = individual[swapped]
-            city2 = individual[swap_with]
-
-            individual[swapped] = city2
-            individual[swap_with] = city1
+            swap_within(individual, swapped)
     return individual
+
+
+def swap_within(individual, swapped):
+    swap_with = int(random.random() * len(individual))
+
+    city1 = individual[swapped]
+    city2 = individual[swap_with]
+
+    individual[swapped], individual[swap_with] = city2, city1
+
+
+indiv = ["a", "b", "c", "d"]
+mr = 0.24
+print(mutate(indiv, mr))
