0606401acc5c85401a00645de3a36316dd4b2008
Girgis Sami <jirgissamy78@gmail.com>
2024-11-29 16:20:22 +0200

    add crossover file

---
+++ crossover.py
@@ -0,0 +1,21 @@
+import random
+
+
+def breed(parent1, parent2):
+    child = []
+    child_p1 = []
+    child_p2 = []
+
+    gene_a = int(random.random() * len(parent1))
+    gene_b = int(random.random() * len(parent1))
+
+    start_gene = min(gene_a, gene_b)
+    end_gene = max(gene_a, gene_b)
+
+    for i in range(start_gene, end_gene):
+        child_p1.append(parent1[i])
+
+    child_p2 = [item for item in parent2 if item not in child_p1]
+
+    child = child_p1 + child_p2
+    return child
