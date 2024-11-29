ffa1ede0148bcc6476d866d7e39f6d2b87a855d1
Girgis Sami <jirgissamy78@gmail.com>
2024-11-29 16:38:01 +0200

    add breed_population

--- crossover.py
+++ crossover.py
@@ -19,3 +19,17 @@ def breed(parent1, parent2):
 
     child = child_p1 + child_p2
     return child
+
+
+def breed_population(mating_pool, elite_size):
+    children = []
+    length = len(mating_pool) - elite_size
+    pool = random.sample(mating_pool, len(mating_pool))
+
+    for i in range(0, elite_size):
+        children.append(mating_pool[i])
+
+    for i in range(0, length):
+        child = breed(pool[i], pool[len(mating_pool) - i - 1])
+        children.append(child)
+    return children
