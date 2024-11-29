036d6a30f812c702cbb235792b09c66f9e9e6ece
Girgis Sami <jirgissamy78@gmail.com>
2024-11-29 19:57:50 +0200

    add index file

---
+++ index.py
@@ -0,0 +1,12 @@
+from crossover import breed_population
+from mutation import mutate_population
+from selection import rank_routes, selection, mating_pool
+
+
+def next_generation(current_gen, elite_size, mutation_rate):
+    pop_ranked = rank_routes(current_gen)
+    selection_results = selection(pop_ranked, elite_size)
+    pool_mating = mating_pool(current_gen, selection_results)
+    children = breed_population(pool_mating, elite_size)
+    result = mutate_population(children, mutation_rate)
+    return result
