aad098b233ab48c139ec2af4473ae8cfb5829fce
Girgis Sami <jirgissamy78@gmail.com>
2024-11-29 19:43:38 +0200

    organize code

--- population.py
+++ population.py
@@ -1,13 +1,13 @@
 import random
 
 
-def create_route(city_list):
-    return random.sample(city_list, len(city_list))
-
-
 def initial_population(pop_size, city_list):
     result = []
 
     for _ in range(0, pop_size):
         result.append(create_route(city_list))
     return result
+
+
+def create_route(city_list):
+    return random.sample(city_list, len(city_list))
