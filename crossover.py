8aa9e48e4373c96f7ef401cd15fc423ab8df7cc9
Girgis Sami <jirgissamy78@gmail.com>
2024-11-29 17:13:12 +0200

    organize code

--- crossover.py
+++ crossover.py
@@ -1,26 +1,6 @@
 import random
 
 
-def breed(parent1, parent2):
-    child = []
-    child_p1 = []
-    child_p2 = []
-
-    gene_a = int(random.random() * len(parent1))
-    gene_b = int(random.random() * len(parent1))
-
-    start_gene = min(gene_a, gene_b)
-    end_gene = max(gene_a, gene_b)
-
-    for i in range(start_gene, end_gene):
-        child_p1.append(parent1[i])
-
-    child_p2 = [item for item in parent2 if item not in child_p1]
-
-    child = child_p1 + child_p2
-    return child
-
-
 def breed_population(mating_pool, elite_size):
     children = breed_elitism(mating_pool, elite_size) + generate_children(
         mating_pool, elite_size
@@ -40,3 +20,23 @@ def generate_children(mating_pool, elite_size):
         child = breed(pool[i], pool[len(mating_pool) - i - 1])
         result.append(child)
     return result
+
+
+def breed(parent1, parent2):
+    child = []
+    child_p1 = []
+    child_p2 = []
+
+    gene_a = int(random.random() * len(parent1))
+    gene_b = int(random.random() * len(parent1))
+
+    start_gene = min(gene_a, gene_b)
+    end_gene = max(gene_a, gene_b)
+
+    for i in range(start_gene, end_gene):
+        child_p1.append(parent1[i])
+
+    child_p2 = [item for item in parent2 if item not in child_p1]
+
+    child = child_p1 + child_p2
+    return child
