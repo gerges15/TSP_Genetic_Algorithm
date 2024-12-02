7e453659f92cd055aba277d70cb4cbfb895e805e
Girgis Sami <jirgissamy78@gmail.com>
2024-12-02 08:24:51 +0200

    replace var with dictionary

--- ga.py
+++ ga.py
@@ -5,7 +5,7 @@ from selection import rank_routes
 
 #! need to refactor make less parameters as you can
 def genetic_algorithm(population, pop_size, elite_size, mutation_rate, generations):
-    tspData = {"population": population}
+    tspData = {"population": population, "pop_size": pop_size}
 
     return zz_genetic_algorithm(
         pop_size, elite_size, mutation_rate, generations, tspData
@@ -13,7 +13,7 @@ def genetic_algorithm(population, pop_size, elite_size, mutation_rate, generatio
 
 
 def zz_genetic_algorithm(pop_size, elite_size, mutation_rate, generations, tspData):
-    pop = initial_population(pop_size, tspData["population"])
+    pop = initial_population(tspData["pop_size"], tspData["population"])
     print("Initial distance: " + str(1 / rank_routes(pop)[0][1]))
 
     for _ in range(0, generations):
