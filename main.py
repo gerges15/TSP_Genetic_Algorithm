cebeb5df5a7a46561a9700d1468c20881e4a9b53
Girgis Sami <jirgissamy78@gmail.com>
2024-12-02 08:32:00 +0200

    inline genetic_algorithm function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 from city import City
-from ga import genetic_algorithm
+from ga import zz_genetic_algorithm
 
 
 cityList = []
@@ -8,10 +8,12 @@ CITY_NUMBERS = 20
 for i in range(0, CITY_NUMBERS):
     cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
 
-best_rout = genetic_algorithm(
-    population=cityList,
-    pop_size=100,
-    elite_size=20,
-    mutation_rate=0.01,
-    generations=50,
-)
+tspData = {
+    "population": cityList,
+    "pop_size": 100,
+    "elite_size": 20,
+    "mutation_rate": 0.01,
+    "generations": 50,
+}
+
+best_rout = zz_genetic_algorithm(tspData)
