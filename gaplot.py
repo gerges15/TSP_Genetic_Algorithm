be38ba7600693791d39868ef1235465023d4c07d
Girgis Sami <jirgissamy78@gmail.com>
2024-12-02 08:38:30 +0200

    replace vlues with objec

--- gaplot.py
+++ gaplot.py
@@ -4,15 +4,13 @@ from population import initial_population
 from selection import rank_routes
 
 
-def genetic_algorithm_plot(
-    population, pop_size, elite_size, mutation_rate, generations
-):
-    pop = initial_population(pop_size, population)
+def genetic_algorithm_plot(tsp_data):
+    pop = initial_population(tsp_data["pop_size"], tsp_data["population"])
     progress = []
     progress.append(1 / rank_routes(pop)[0][1])
 
-    for _ in range(0, generations):
-        pop = next_generation(pop, elite_size, mutation_rate)
+    for _ in range(0, tsp_data["generations"]):
+        pop = next_generation(pop, tsp_data["elite_size"], tsp_data["mutation_rate"])
         progress.append(1 / rank_routes(pop)[0][1])
 
     plt.plot(progress)
