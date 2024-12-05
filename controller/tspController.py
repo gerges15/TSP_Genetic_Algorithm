2c97d5238d2369fe63da719aa3e8c7baf344b3a5
Girgis Sami <jirgissamy78@gmail.com>
2024-12-05 19:55:53 +0200

    add color to continue_button

--- controller/tspController.py
+++ controller/tspController.py
@@ -118,6 +118,8 @@ class TSPApp:
             text="Continue",
             command=self.continue_algorithm,
             font=self.font,
+            bg="#6A42C2",
+            fg="white",
         )
         continue_button.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
 
