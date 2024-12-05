4bd278e18998cbb6fa52b9d1335f580439c8a998
Girgis Sami <jirgissamy78@gmail.com>
2024-12-05 16:59:32 +0200

    change logic

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from model.city import generate_city_list
-from model.ga import genetic_algorithm
+from controller.tspController import TSPController
 
-CITY_NUMBERS = 20
-
-tsp_data = {
-    "population": generate_city_list(CITY_NUMBERS),
-    "pop_size": 100,
-    "elite_size": 20,
-    "mutation_rate": 0.01,
-    "generations": 5,
-}
-
-best_rout = genetic_algorithm(tsp_data)
+if __name__ == "__main__":
+    controller = TSPController()
